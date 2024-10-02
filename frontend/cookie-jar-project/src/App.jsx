bf943e5d5ecd205fefdc775c4d364ca25f85cd1d
Chimaezss <cheesewithease@outlook.com>
2024-10-02 18:02:00 -0400

    vite app setup and start of homepage

---
+++ frontend/cookie-jar-project/src/App.jsx
@@ -0,0 +1,9 @@
+import Homepage from './components/homepage/homepage';
+
+function App() {
+  return (
+    <Homepage />
+  );
+}
+
+export default App
---
+++ frontend/cookie-jar-project/src/components/homepage/homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import cookieJarLogo from '../../assets/logo.jpg'
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+            <img src={cookieJarLogo} alt="The Cookie Jar Logo"/>
+        </div>
+    );
+};
+
+export default Homepage
