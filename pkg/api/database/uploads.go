d19eca48aa17f853ff0461aef1a1a0511511fb4f
Michael spikes <mkspikes@gmail.com>
2024-11-19 23:49:27 -0500

    - Added a media controller which serves the media, and handles writing media to the disk for storage -Updated format of media structure to include typical metadata (size, path, time created), keeping the username and unique id associated with it -Added routes to router.go for testing with curl

--- pkg/api/database/uploads.go
+++ pkg/api/database/uploads.go
@@ -1,54 +1,47 @@
 package database
 
-impott (
-    "context"
-    "fmt"
+import (
+	"context"
+	"fmt"
 
-    "github.com/danmuck/the_cookie_jar/pkg/api/models"
-    "github.com/gin-gonic/gin"
+	"github.com/danmuck/the_cookie_jar/pkg/api/models"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
-// This is just a simple mapping 
-var ValidFormats = map[string]bool{
-    "jpeg": true,
-    "png":  true,
-    "webp": true,
-    "gif":  true,
- }
- 
- // AddMedia adds new media to database. Media types have a UUID, an asssociated username, and a format, which is a map of valid formats to boolean values.
- func AddMedia(uuid string, username string, format string) error {
-    if !ValidFormats[format] {
-        return fmt.Errorf("unsupported format: %s", format)
-    }
- 
-    media := &models.Media{
-        ID:       uuid,
-        Username: username, 
-        Format:   format,
-    }
- 
-    _, err := GetCollection("media").InsertOne(context.TODO(), media)
-    if err != nil {
-        return fmt.Errorf("failed to upload media: %w", err)
-    }
- 
-    return nil
- }
- //This searches for a media upload by id. Returns the media if it is found.
- func GetMedia(id string) (*models.Media, error) {
-    var media *models.Media
-    err := GetCollection("media").FindOne(context.TODO(), gin.H{"_id": id}).Decode(&media)
-    return media, err
- }
- //This will search the DB for the media by id, returning an error if it is not found.
- func DeleteMedia(id string) error {
-    result, err := GetCollection("media").DeleteOne(context.TODO(), gin.H{"_id": id})
-    if err != nil {
-        return err
-    }
-    if result.DeletedCount == 0 {
-        return fmt.Errorf("media not found: %s", id)
-    }
-    return nil
- }
+// AddMedia adds new media to database
+func AddMedia(id string, username string, path string, size int64) error {
+	media := &models.Media{
+		ID:       id,
+		Username: username,
+		Path:     path,
+		Size:     size,
+	}
+
+	_, err := GetCollection("media").InsertOne(context.TODO(), media)
+	if err != nil {
+		return fmt.Errorf("failed to insert media: %w", err)
+	}
+	return nil
+}
+
+// GetMedia retrieves media by ID
+func GetMedia(id string) (*models.Media, error) {
+	var media models.Media
+	err := GetCollection("media").FindOne(context.TODO(), bson.M{"_id": id}).Decode(&media)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch media: %w", err)
+	}
+	return &media, nil
+}
+
+// DeleteMedia removes media from database
+func DeleteMedia(id string) error {
+	result, err := GetCollection("media").DeleteOne(context.TODO(), bson.M{"_id": id})
+	if err != nil {
+		return err
+	}
+	if result.DeletedCount == 0 {
+		return fmt.Errorf("media not found: %s", id)
+	}
+	return nil
+}
--- pkg/api/models/media.go
+++ pkg/api/models/media.go
@@ -1,7 +1,12 @@
 package models
 
+import "time"
+
 type Media struct {
-    ID       string `bson:"_id" json:"id"`
-    Username string `bson:"username" json:"username"`
-    Format   string `bson:"format" json:"format"`
+    ID        string    `bson:"_id" json:"id"`
+    Username  string    `bson:"username" json:"username"`
+    Format    string    `bson:"format" json:"format"`
+    Path      string    `bson:"path" json:"path"`
+    Size      int64     `bson:"size" json:"size"`
+    CreatedAt time.Time `bson:"created_at" json:"created_at"`
 }
